3b2687edc63cdf6e0bb3f87d5251cee0713fba19
Arely Rivera <arelyrivera1996@gmail.com>
2023-05-28 21:55:17 -0700

    added card game, problem 2 API call, and problem 6 leetcode longest substring

--- asessment.js
+++ asessment.js
@@ -16,14 +16,18 @@ complete the code as per the given instructions:
 
 class APICaller extends React.Component{
   callApi(){
+    const url = "https://www.reddit.com/r/react.json";
     //#1 Implement a fetch method with the given API ENDPOINT
     // YOUR CODE HERE 
+    fetch(url)
     .then((result)=>{
       //#2 Return the result in json format
       //YOUR CODE HERE 
+      return result.json();
     }).then((jsonData)=>{
       //#3 Print/log the jsonData in the console of the browser
       //YOUR CODE HERE 
+      console.log(jsonData);
     })
   }
 render(){
@@ -31,6 +35,7 @@ render(){
     <button 
   //#4 Implement an onCLick functionality to the button such that it calls the callApi() function when it is clicked. 
   // YOUR CODE HERE 
+    onClick={this.callApi}
     >Call the API now.</button>
   </div>
 }
@@ -38,7 +43,7 @@ render(){
 React.render(
   //#5 Implement the APICaller component appropiately into the render method
   //YOUR CODE HERE 
-  , document.getElementById('myapicaller')
+  <APICaller />, document.getElementById('myapicaller')
 )
 
 
@@ -83,6 +88,57 @@ solve the following leetcode in JavaScript:
 https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
 
 and paste your solution here. Please comment each line of your code to explain it, and be prepared to explain in the follow up interview.
+*/
+var longestSubstring = function(s, k) {
+    // split s into an array
+    const sArray = s.split(""); // [a,b,a,b,b,c]
+
+    // variable to hold letters that are okay to have in substring 
+    let okLetters = []; // [a, b]
+
+    // variable to hold the highest substring count
+    let highestCount = 0;
+
+    // variable to hold current substring count
+    let currentCount = 0;
+
+    // create a hashmap of all the letters and how many times they occur
+    let hash = new Map();
+
+    for (let i = 0; i < sArray.length; i++) {
+        // if the letter is not in the hashmap, add it to the hashmap
+        if (!hash.has(sArray[i])) {
+            hash.set(sArray[i], 1)
+        } else {
+            // if it does exist in the hashmap then get the hash value and add one to it
+            let currentValue = hash.get(sArray[i]) + 1;
+            hash.set(sArray[i], currentValue)
+        }
+
+        // if the letter in the hash is greater than k, add it to the okLetter array
+        if (hash.get(sArray[i]) >= k && !okLetters.includes(sArray[i])) {
+            okLetters.push(sArray[i]);
+        }
+    }
+
+    // loop through the array s, keeping count of the highest substring count and the current substring count
+    for (let i = 0; i < sArray.length; i++) {
+        if (okLetters.includes(sArray[i])) {
+            currentCount = currentCount + 1;
+            console.log("currentCount = "+ currentCount)
+            if (currentCount >= highestCount) {
+                highestCount = currentCount 
+                console.log(highestCount)
+            }
+        } else {
+            currentCount = 0; 
+        }
+    }
+
+    // return highest substring length
+    return(highestCount)
+};
+/*
 
 /* Problem 7 */
 /*Skill: SQL
--- cardGame.js
+++ cardGame.js
@@ -1,29 +1,119 @@
-/* 
-Overview 
+// Overview
 
-Create a JS file that uses classes to create an object representing a deck of cards. Then create a simple console version of the card game 'War'
+// Create a JS file that uses classes to create an object representing a deck of cards. Then create a simple console version of the card game 'War'
 
-Steps: 
+// Steps:
 
-1. Create a Deck class 
+// 1. Create a Deck class
+class Deck {
+  constructor(suit, name, value) {
+    this.suit = suit;
+    this.name = name;
+    this.value = value;
+  }
+  // 2. Create a method on the Deck class that dynamically generates each card object
+  // example card object: {suit: "hearts", value: 2}
 
-2. Create a method on the Deck class that dynamically generates each card object
-example card object: {suit: "hearts", value: 2}
+  // a card's value corresponds to the number/face card's rank
 
-a card's value corresponds to the number/face card's rank
+  // ace = 1
+  // cards 2-10 = their number value
+  // jacks = 11
+  // queens = 12
+  // kings = 13
+  generateDeck() {
+    const suitArray = ["hearts", "diamonds", "spades", "clubs"];
+    const nameArray = "ace 2 3 4 5 6 7 8 9 10 jack queen king".split(" ");
+    // 3. card objects can be held inside an array
+    const deck = [];
 
-ace = 1
-cards 2-10 = their number value 
-jacks = 11 
-queens = 12 
-kings = 13 
+    for (let i = 0; i < suitArray.length; i++) {
+      for (let j = 0; j < nameArray.length; j++) {
+        let currentValue;
 
-3. card objects can be held inside an array 
+        switch (nameArray[j]) {
+          case "ace":
+            currentValue = 1;
+            break;
+          case "2":
+            currentValue = 2;
+            break;
+          case "3":
+            currentValue = 3;
+            break;
+          case "4":
+            currentValue = 4;
+            break;
+          case "5":
+            currentValue = 5;
+            break;
+          case "6":
+            currentValue = 6;
+            break;
+          case "7":
+            currentValue = 7;
+            break;
+          case "8":
+            currentValue = 8;
+            break;
+          case "9":
+            currentValue = 9;
+            break;
+          case "10":
+            currentValue = 10;
+            break;
+          case "jack":
+            currentValue = 11;
+            break;
+          case "queen":
+            currentValue = 12;
+            break;
+          case "king":
+            currentValue = 13;
+            break;
+        }
 
-4. create a method to select a random card object 
+        deck.push({
+          suit: suitArray[i],
+          name: nameArray[j],
+          value: currentValue,
+        });
+      }
+    }
 
-5. create a function that runs the random card method on each deck, and compares the values of the 2 random cards
+    return deck;
+  }
 
-6. console log both card objects, and a message declaring which one has the higher value
+  // 4. create a method to select a random card object
+  getRandomCard(deck) {
+    return deck[Math.floor(Math.random() * deck.length)];
+  }
+}
 
-*/
+// 5. create a function that runs the random card method on each deck, and compares the values of the 2 random cards
+function playWar() {
+  let deck1 = new Deck().generateDeck();
+  let deck2 = new Deck().generateDeck();
+  let player1Card = new Deck().getRandomCard(deck1);
+  let player2Card = new Deck().getRandomCard(deck2);
+
+  console.log("Player 1 : " + player1Card);
+  console.log("Player 2 : " + player2Card);
+
+  if (player1Card.value > player2Card.value) {
+    console.log(
+      `Player 1 Wins with a ${player1Card.name} of ${player1Card.suit}! Player 2 loses with a ${player2Card.name} of ${player2Card.suit}`
+    );
+  } else if (player1Card.value < player2Card.value) {
+    console.log(
+      `Player 2 Wins with a ${player2Card.name} of ${player2Card.suit}! Player 1 loses with a ${player1Card.name} of ${player1Card.suit}`
+    );
+  } else {
+    console.log(
+      `Player 1 and Player 2 have a tie! Player 1 has a ${player1Card.name} of ${player1Card.suit} and Player 2 has a ${player2Card.name} of ${player2Card.suit}`
+    );
+  }
+}
+
+// 6. console log both card objects, and a message declaring which one has the higher value
+playWar();
